d6634f869a6d8e8410f9752a146cfb9d19d77890
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2018-11-18 13:21:14 -0500

    Coffee Model done using sequelize

--- server/models/coffee.model.js
+++ server/models/coffee.model.js
@@ -1,8 +1,41 @@
 const Sequelize = require('sequelize')
 const db = require('./database')
+const Op = Sequelize.Op;
 
 const Coffee = db.define('coffee', {
   // your code here
-})
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  ingredients: Sequelize.ARRAY(Sequelize.STRING)
+});
 
-module.exports = Coffee
+// Adds love to all drinks <3
+Coffee.beforeValidate((instance, options) =>{
+  const ing = instance.dataValues.ingredients;
+  if(!ing.includes('love')){
+    ing.push('love');
+  }
+});
+
+
+
+// Gets all ingredients of an instance 
+Coffee.prototype.getIngredients = function(){
+  return this.ingredients.join(', ');
+};
+
+// Finds all coffees with a given ingredient 
+Coffee.findByIngredient = async function (ingredient) {
+  const coffeeWithIng = await Coffee.findAll({
+    where:{
+      ingredients: {
+        [Op.contains]: [ingredient],
+      }
+    }
+  });
+  return coffeeWithIng;
+};
+
+module.exports = Coffee;
